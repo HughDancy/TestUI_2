446e625a8e963b09f97499fee04fc72d38c489cd
BorisKiselev <hugh.dancy@icloud.com>
2022-05-14 11:43:34 +0700

    Work with AppDelegate and SceneDelegate methods
    Добавлены основные методы в AppDelegate и SceneDelegate с выводом соответствующих принтов

--- TestUI_2/AppDelegate.swift
+++ TestUI_2/AppDelegate.swift
@@ -11,14 +11,24 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print("Начинается запуск")
         return true
     }
 
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        print("Основная настройка приложения")
+        return true
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        print("Приложение выгружено")
+    }
     // MARK: UISceneSession Lifecycle
-
+    
+   
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
